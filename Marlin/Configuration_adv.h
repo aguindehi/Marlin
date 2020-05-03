ad6102a0e72d5ad3ff7803da54f889d7dd0104fb
Amir Guindehi <amir@datacore.ch>
2020-05-03 03:04:29 +0200

    Fixed small errors. Switched to LPC1768. Compiles successfult.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -429,7 +429,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN -1
+//#define E0_AUTO_FAN_PIN -1
 #define E0_AUTO_FAN_PIN P2_04
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
@@ -2193,7 +2193,7 @@
       #define Z_CURRENT     600
     #endif
     #define Z_CURRENT_HOME  Z_CURRENT
-    #define Z_MICROSTEPS     16
+    //#define Z_MICROSTEPS     16
     #define Z_MICROSTEPS     SK_USTEPS
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
