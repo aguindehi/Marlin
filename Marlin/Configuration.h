ad3d32b9d6ae7c064ab673fd794a16a4f6fe262d
Amir Guindehi <amir@datacore.ch>
2020-05-06 14:59:43 +0200

    configured UUID and author.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -118,7 +118,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Amir Guindehi <amir@datacore.ch>, SecKit SK-Go config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -191,6 +191,7 @@
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
 //#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "17bcd441-359c-4a12-bb1f-419d8b38f629"
 
 // @section extruder
 
