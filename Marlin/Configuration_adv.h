80cca8b6ae979fc1880c6f79c6324b7ed9570a6b
Amir Guindehi <amir@datacore.ch>
2020-12-25 15:21:14 +0100

    enable IMPROVE_HOMING_RELIABILITY and set ?_CURRENT_HOME (original a301db4).

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2128,8 +2128,8 @@
  */
 #define ADVANCED_PAUSE_FEATURE
 #if ENABLED(ADVANCED_PAUSE_FEATURE)
-  //#define PAUSE_PARK_RETRACT_FEEDRATE         60  // (mm/s) Initial retract feedrate.
   #define PAUSE_PARK_RETRACT_FEEDRATE         30  // (mm/s) Initial retract feedrate.
+// #define PAUSE_PARK_RETRACT_FEEDRATE         6  // (mm/s) Initial retract feedrate. for SK-Swift
   #define PAUSE_PARK_RETRACT_LENGTH            2  // (mm) Initial retract.
                                                   // This short retract is done immediately, before parking the nozzle.
   //#define FILAMENT_CHANGE_UNLOAD_FEEDRATE     10  // (mm/s) Unload filament feedrate. This can be pretty fast.
@@ -2146,16 +2146,14 @@
     #define FILAMENT_CHANGE_UNLOAD_LENGTH      100
   #endif
 
-  //#define FILAMENT_CHANGE_SLOW_LOAD_FEEDRATE   6  // (mm/s) Slow move when starting load.
-  #define FILAMENT_CHANGE_SLOW_LOAD_FEEDRATE   10  // (mm/s) Slow move when starting load.
-  //#define FILAMENT_CHANGE_SLOW_LOAD_LENGTH     0  // (mm) Slow length, to allow time to insert material.
-  #define FILAMENT_CHANGE_SLOW_LOAD_LENGTH     80  // (mm) Slow length, to allow time to insert material.
+  #define FILAMENT_CHANGE_SLOW_LOAD_FEEDRATE   10 // (mm/s) Slow move when starting load.
+  // #define FILAMENT_CHANGE_SLOW_LOAD_FEEDRATE   4  // for SK-Swift. (mm/s) Slow move when starting load.
+  #define FILAMENT_CHANGE_SLOW_LOAD_LENGTH     30 // (mm) Slow length, to allow time to insert material.
                                                   // 0 to disable start loading and skip to fast load only
-  //#define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE   6  // (mm/s) Load filament feedrate. This can be pretty fast.
-  #define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE  15  // (mm/s) Load filament feedrate. This can be pretty fast.
-  //#define FILAMENT_CHANGE_FAST_LOAD_ACCEL     25  // (mm/s^2) Lower acceleration may allow a faster feedrate.
-  #define FILAMENT_CHANGE_FAST_LOAD_ACCEL     20  // (mm/s^2) Lower acceleration may allow a faster feedrate.
-  //#define FILAMENT_CHANGE_FAST_LOAD_LENGTH     0  // (mm) Load length of filament, from extruder gear to nozzle.
+  #define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE   15 // (mm/s) Load filament feedrate. This can be pretty fast.
+  // #define FILAMENT_CHANGE_FAST_LOAD_FEEDRATE   6  // for SK-Swift. (mm/s) Load filament feedrate. This can be pretty fast.
+  #define FILAMENT_CHANGE_FAST_LOAD_ACCEL      20 // (mm/s^2) Lower acceleration may allow a faster feedrate.
+  // #define FILAMENT_CHANGE_FAST_LOAD_LENGTH     0  // (mm) Load length of filament, from extruder gear to nozzle.
                                                   //   For Bowden, the full length of the tube and nozzle.
                                                   //   For direct drive, the full length of the nozzle.
   #ifdef BOWDEN_EXTRUSION
@@ -2165,10 +2163,9 @@
   #endif
 
   //#define ADVANCED_PAUSE_CONTINUOUS_PURGE       // Purge continuously up to the purge length until interrupted.
-  //#define ADVANCED_PAUSE_PURGE_FEEDRATE        3  // (mm/s) Extrude feedrate (after loading). Should be slower than load feedrate.
+  #define ADVANCED_PAUSE_PURGE_FEEDRATE        3  // (mm/s) Extrude feedrate (after loading). Should be slower than load feedrate.
   #define ADVANCED_PAUSE_PURGE_FEEDRATE       10  // (mm/s) Extrude feedrate (after loading). Should be slower than load feedrate.
-  //#define ADVANCED_PAUSE_PURGE_LENGTH         50  // (mm) Length to extrude after loading.
-  #define ADVANCED_PAUSE_PURGE_LENGTH         5  // (mm) Length to extrude after loading.
+  #define ADVANCED_PAUSE_PURGE_LENGTH          5  // (mm) Length to extrude after loading.
                                                   //   Set to 0 for manual extrusion.
                                                   //   Filament can be extruded repeatedly from the Filament Change menu
                                                   //   until extrusion is consistent, and to purge old filament.
@@ -2176,10 +2173,11 @@
   //#define ADVANCED_PAUSE_FANS_PAUSE             // Turn off print-cooling fans while the machine is paused.
 
                                                   // Filament Unload does a Retract, Delay, and Purge first:
-  #define FILAMENT_UNLOAD_PURGE_RETRACT       13  // (mm) Unload initial retract length.
+  #define FILAMENT_UNLOAD_PURGE_RETRACT       10  // (mm) Unload initial retract length.
   #define FILAMENT_UNLOAD_PURGE_DELAY       5000  // (ms) Delay for the filament to cool after retract.
   #define FILAMENT_UNLOAD_PURGE_LENGTH         8  // (mm) An unretract is done, then this length is purged.
   #define FILAMENT_UNLOAD_PURGE_FEEDRATE      25  // (mm/s) feedrate to purge before unload
+  // #define FILAMENT_UNLOAD_PURGE_FEEDRATE      6  // for SK-Swift. (mm/s) feedrate to purge before unload
 
   #define PAUSE_PARK_NOZZLE_TIMEOUT           45  // (seconds) Time limit before the nozzle is turned off for safety.
   #define FILAMENT_CHANGE_ALERT_BEEPS         10  // Number of alert beeps to play when a response is needed.
@@ -2333,8 +2331,10 @@
     #ifdef BOWDEN_EXTRUSION
       #define X_CURRENT     800  // (mA) RMS current. Multiply by 1.414 for peak current.
     #else
-      #define X_CURRENT     800  // (mA) RMS current. Multiply by 1.414 for peak current.
+      //#define X_CURRENT     800  // (mA) RMS current. Multiply by 1.414 for peak current.
+      #define X_CURRENT     400  // (mA) RMS current. Multiply by 1.414 for peak current.
     #endif
+    //#define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     //#define X_MICROSTEPS     16    // 0..256
     #define X_MICROSTEPS     SK_USTEPS    // 0..256
@@ -2357,7 +2357,8 @@
     #ifdef BOWDEN_EXTRUSION
       #define Y_CURRENT     800  // (mA) RMS current. Multiply by 1.414 for peak current.
     #else
-      #define Y_CURRENT     800
+      //#define Y_CURRENT     800
+      #define Y_CURRENT     400
     #endif
     #define Y_CURRENT_HOME  Y_CURRENT
     //#define Y_MICROSTEPS     16
@@ -2421,6 +2422,7 @@
   #if AXIS_IS_TMC(E0)
     //#define E0_CURRENT      800
     #define E0_CURRENT      550
+    // #define E0_CURRENT      500   // for SK-Swift
     //#define E0_MICROSTEPS    16
     #define E0_MICROSTEPS    SK_USTEPS
     #define E0_RSENSE         0.11
@@ -2712,7 +2714,7 @@
 
     //#define Z_STALL_SENSITIVITY  8
     //#define SPI_ENDSTOPS              // TMC2130 only
-    //#define IMPROVE_HOMING_RELIABILITY
+    #define IMPROVE_HOMING_RELIABILITY
   #endif
 
   /**
