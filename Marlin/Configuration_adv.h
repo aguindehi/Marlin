429b7828170f9648901c73befe41dd6f527998b5
Amir Guindehi <amir@datacore.ch>
2021-02-26 23:38:25 +0100

    cleanup in homing section for SK_YX_HOMING_ENDSTOPS=true.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -654,29 +654,23 @@
  * Homing (G28) does an indefinite move towards the endstops to establish
  * the position of the toolhead relative to the workspace.
  */
-
 #if SK_YX_HOMING_ENDSTOPS
-  //#define HOMING_BUMP_MM      { 2, 2, 2 }   // (mm) Backoff from endstops after first bump
-  #define HOMING_BUMP_MM      { 5, 5, 2 }     // (mm) Backoff from endstops after first bump
-  #define HOMING_BUMP_DIVISOR { 4, 4, 4 }     // Re-Bump Speed Divisor (Divides the Homing Feedrate)
+  //#define HOMING_BUMP_MM       { 2, 2, 2 }  // (mm) Backoff from endstops after first bump
+  #define HOMING_BUMP_MM         { 5, 5, 2 }  // (mm) Backoff from endstops after first bump
+  #define HOMING_BUMP_DIVISOR    { 4, 4, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
   #define HOMING_BACKOFF_POST_MM { 0, 0, 2 }  // (mm) Backoff from endstops after homing
+
+  #define HOME_Y_BEFORE_X                     // If G28 contains XY home Y before X
+  #define CODEPENDENT_XY_HOMING               // Make sure Marlin is aware that homing X depends on homing Y first
 #else /* SK_YX_HOMING_ENDSTOPS */
   #define SENSORLESS_BACKOFF_MM  { 2, 2 }     // (mm) Backoff from endstops before sensorless homing
-  #define HOMING_BUMP_MM      { 0, 0, 0 }     // (mm) Backoff from endstops after first bump (Ernest switches this off as recommended in the src file)
-  #define HOMING_BUMP_DIVISOR { 2, 2, 4 }     // Re-Bump Speed Divisor (Divides the Homing Feedrate)
+  #define HOMING_BUMP_MM         { 0, 0, 0 }  // (mm) Backoff from endstops after first bump (Ernest switches this off as recommended in the src file)
+  #define HOMING_BUMP_DIVISOR    { 2, 2, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
   #define HOMING_BACKOFF_POST_MM { 2, 5, 2 }  // (mm) Backoff from endstops after homing
 #endif /* SK_YX_HOMING_ENDSTOPS */
 
-
-
 //#define QUICK_HOME                          // If G28 contains XY do a diagonal move first
-#if SK_YX_HOMING_ENDSTOPS
-  #define HOME_Y_BEFORE_X                     // If G28 contains XY home Y before X
-#endif
 //#define HOME_Z_FIRST                        // Home Z first. Requires a Z-MIN endstop (not a probe).
-#if SK_YX_HOMING_ENDSTOPS
-  #define CODEPENDENT_XY_HOMING
-#endif
 
 // @section bltouch
 
